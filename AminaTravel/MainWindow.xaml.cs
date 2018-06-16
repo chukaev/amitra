94534743f03247ecbf3d63160bd09be2172518b0
Чукаев Юрий Сергеевич <Chukaev@LIVE.RU>
2018-06-16 16:55:38 +0300

    Main Window with Mohups style

--- AminaTravel/MainWindow.xaml.cs
+++ AminaTravel/MainWindow.xaml.cs
@@ -12,17 +12,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace AminaTravel
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        public bool FirstClick { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            //TourAdressTextBox.Text
+            FirstClick = !FirstClick;
+
+            if (FirstClick)
+            {
+                Ring.IsActive = true;
+                FindingLabel.Visibility = Visibility.Visible;
+                ResultsTab.Visibility = Visibility.Hidden;
+                ComparisonGrid.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Ring.IsActive = false;
+                FindingLabel.Visibility = Visibility.Hidden;
+                ResultsTab.Visibility = Visibility.Visible;
+                ComparisonGrid.Visibility = Visibility.Visible;
+            }
         }
     }
 }
